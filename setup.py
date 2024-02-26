b88fbb0762c6a954538fa2fa9e815575e4831e18
Daniel F <154395983+dtflare@users.noreply.github.com>
2024-02-26 15:26:09 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parsR",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "langchain==0.1.4",
+        "openai==1.10.0",
+        "deeplake",
+        "tiktoken",
+        "newspaper3k",
+        "python-dotenv"
+    ],
+    python_requires='>=3.8',
+)

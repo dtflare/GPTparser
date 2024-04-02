45bd6549fa138a94ed41b28419f9a637f9a61b44
Daniel F <154395983+dtflare@users.noreply.github.com>
2024-04-01 23:58:53 -0400

    Update setup.py
    removing msg_array script, will add it back later with added functionality.

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
             'GPTparser=GPTparser.GPTparser:main',
             'combinR=GPTparser.extra_scripts.combinR:main',
             'j_val=GPTparser.extra_scripts.j_val:main',
-            'msg_array=GPTparser.extra_scripts.msg_array:main',
             'wordcount=GPTparser.extra_scripts.wordcount:main',
         ],
     },
